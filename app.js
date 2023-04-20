0859c753ea8e2ed1fcb4595c9991299482f98296
Aaron Clark <amclarkwustl@gmail.com>
2023-04-20 14:20:16 -0700

    refactored JS to reprompt user for name

--- app.js
+++ app.js
@@ -17,3 +17,28 @@ dark.addEventListener("click", function(){
     }
 });
 
+const renButton = document.getElementById("name-change");
+const heading = document.getElementById("heading");
+
+// isValid function adapted from here: https://codingbeautydev.com/blog/javascript-check-if-string-contains-only-letters-and-spaces/#:~:text=test()%20Method-,To%20check%20if%20a%20string%20contains%20only%20letters%20and%20spaces,Otherwise%2C%20it%20returns%20false%20.
+function isValid(str) {
+    return /^[A-Za-z\s]*$/.test(str);
+};
+
+renButton.addEventListener("click", function(){
+    let name = ""
+    while (true) {
+        name = prompt("Please enter your name:");
+        if (isValid(name) & name != null) {
+            break;
+        }
+        else if (name == null) {
+            console.log("User cancelled name input")
+            return 1;
+        }
+        else {
+            alert("Invalid input.  Please enter only letters and spaces");
+        }
+    }
+    heading.textContent = name + " Renegade Leader";
+});
